85f251fe9c9056b685732be8d04733467b1b5480
tebogoditse <tebogoditse2@gmail.com>
2026-01-15 17:01:31 +0200

    1_nested_pattern.cs

---
+++ Day1/1_nested_pattern.cs
@@ -0,0 +1,27 @@
+using System;
+
+class NestedPattern
+{
+
+    public int size;
+    static void PrintNestedPattern()
+    {
+        NestedPattern n = new NestedPattern();
+        n.size = int.Parse(Console.ReadLine());
+
+        for (int row = 1; row <= n.size; row++)
+        {
+            for (int col = 1; col <= n.size; col++)
+            {
+                Console.Write(row);
+                Console.Write(' ');
+            }
+            Console.Write('\n');
+        }
+    }
+
+    static void Main()
+    {
+        PrintNestedPattern();
+    }
+}
