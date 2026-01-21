0ae06675ce5bf9c0ec419a726464505012ec834d
tebogoditse <tebogoditse2@gmail.com>
2026-01-21 23:13:44 +0200

    7_split.cs

---
+++ Day2/7_split.cs
@@ -0,0 +1,45 @@
+using System;
+
+class SplitStr
+{
+    public string beforeSplit;
+
+    static void SplitAndPrint()
+    {
+        SplitStr str = new SplitStr();
+        str.beforeSplit = Console.ReadLine();
+
+        string[] afterSplit = str.beforeSplit.Split(',');
+
+        int i = 0;
+        for (; i < afterSplit.Length; i++)
+        {
+            Console.Write(afterSplit[i] + '\n');
+        }
+    }
+
+    static void Main()
+    {
+        SplitAndPrint();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//apple,banana,cherry
