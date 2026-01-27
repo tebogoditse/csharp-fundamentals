b33227aec20a31b0339d1a6d107a0bfa0e8b4332
tebogoditse <tebogoditse2@gmail.com>
2026-01-27 22:58:11 +0200

    calculate factorial

---
+++ Day0/4_factorial.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Factorial
+{
+
+    public int num;
+
+    static void Fact()
+    {
+        Factorial input = new Factorial();
+        input.num = int.Parse(Console.ReadLine());
+
+        int factorial = 1;
+
+        for (; input.num > 0; input.num--)
+        {
+            factorial = factorial * input.num;
+        }
+
+        Console.Write(factorial);
+
+    }
+    static void Main()
+    {
+        Fact();
+    }
+
+}
