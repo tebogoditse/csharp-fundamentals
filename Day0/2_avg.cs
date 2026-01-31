5b3ad82629245c7908554af83c6f7573e5f7bf7d
tebogoditse <tebogoditse2@gmail.com>
2026-01-31 16:53:37 +0200

    2_avg.cs

---
+++ Day0/2_avg.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        float num1 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        float num2 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        float num3 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        float average = (num1 + num2 + num3) / 3;
+        Console.Write("Average: " + average.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
