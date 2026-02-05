fba399cf85427e8c5eca13a507ac0382fdd077cd
tebogoditse <tebogoditse2@gmail.com>
2026-02-05 11:51:01 +0200

    6_fibonacci.cs

---
+++ Day0/6_fibonacci.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+class Fibonacci
+{
+    public int number;
+    
+    static void Fib()
+    {
+        List<int> numbers = new List<int>();
+
+        numbers.Add(0);
+        numbers.Add(1);
+
+        Fibonacci end = new Fibonacci();
+        end.number = int.Parse(Console.ReadLine());
+
+        if (end.number < 2)
+            return;
+
+        int i = 2;
+        for (; i < end.number; i++)
+        {
+            int next = numbers[numbers.Count - 1] + numbers[numbers.Count - 2];
+            numbers.Add(next);
+        }
+
+        int count = 0;
+        for (; count < numbers.Count; count++)
+        {
+            Console.Write(numbers[count] + " ");
+        }
+
+ 
+    }
+
+    static void Main()
+    {
+        Fib();
+    }
+}
