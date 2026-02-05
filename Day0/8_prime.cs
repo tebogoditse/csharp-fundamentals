c01c1e7e5ee29e67d1498dc02c3b06e72f15fe37
tebogoditse <tebogoditse2@gmail.com>
2026-02-06 00:35:26 +0200

    8_prime.cs

---
+++ Day0/8_prime.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Prime
+{
+    public int number;
+    
+    static bool isPrime()
+    {
+        Prime check = new Prime();
+        check.number = int.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+
+        if (check.number < 2)
+        {
+            isPrime = false;
+        }
+
+        int i = 2;
+        for (; i <= check.number / 2; i++)
+        {
+            if (check.number % i == 0)
+            {
+                isPrime =  false;
+            }
+        }
+
+        return isPrime;
+    }
+
+    static void Main()
+    {
+
+        Console.Write(isPrime() ? "Prime" : "Not Prime");
+       
+    }
+}
