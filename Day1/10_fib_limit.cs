acd10957f8c37313a3c65b09533de3a3588b19d1
tebogoditse <tebogoditse2@gmail.com>
2026-02-17 01:42:28 +0200

    10_fib_limit.cs

---
+++ Day1/10_fib_limit.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+class FibLimit
+{
+    int number;
+
+    static void PrintFibonacciUpTo()
+    {
+        List<int> list = new List<int>();
+
+        list.Add(0);
+        list.Add(1);
+
+        FibLimit n = new FibLimit();
+        int limit = n.number = int.Parse(Console.ReadLine());
+
+        if (limit == 0)
+        {
+            return;
+        }
+
+        int next;
+
+        int i = 2;
+        for (; list[list.Count - 1] + list[list.Count - 2] <= limit; i++)
+        {
+            next = list[list.Count - 1] + list[list.Count - 2];
+            list.Add(next);
+        }
+
+        int counter = 0;
+        for (; counter < list.Count; counter++)
+        {
+            Console.Write(list[counter] + " ");
+        }
+    }
+
+    static void Main()
+    {
+        PrintFibonacciUpTo();
+    }
+}
