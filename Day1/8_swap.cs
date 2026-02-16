f712e584980e1ba04ade427db6276efcb5615eda
tebogoditse <tebogoditse2@gmail.com>
2026-02-16 15:58:30 +0200

    8_swap.cs

---
+++ Day1/8_swap.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Swap
+{
+    int num1, num2;
+
+    static void SwapNumbers()
+    {
+        Swap n = new Swap();
+
+        Console.Write("a = ");
+        int a = n.num1 = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        int b = n.num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Before: a=" + a + ", b=" + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine("After: a=" + a + ", b=" + b);
+    }
+
+    static void Main()
+    {
+
+        SwapNumbers();
+    }
+}
